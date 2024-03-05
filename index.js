94944b8a1a1dcf25618e815b8aa7e8a8d53128fd
Gabrielle <ms.grodarte@gmail.com>
2024-03-05 13:41:03 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+class Polygon {
+    constructor(array){
+        this.sideLengths = array
+    }
+
+    get countSides(){
+        return this.sideLengths.length
+    }
+
+    get perimeter(){
+        return this.sideLengths.reduce((accum, side) => {
+            return accum += side
+        }, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        const [a, b, c] = this.sideLengths
+        return (a+b>c && b+c>a && a+c>b)
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        const [a,b,c,d] = this.sideLengths
+        return (a===b && a===c && a===d)
+    }
+
+    get area(){
+        const [a,b,c,d] = this.sideLengths
+        return (a*b)
+    }
+}
